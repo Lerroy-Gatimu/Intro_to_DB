54d38abd09669b09f55fe6f603c7ae68ee79e4b2
Lerroy Gatimu <lerroygatimu@gmail.com>
2025-10-19 12:18:57 +0300

    Fix case sensitivity in SHOW CREATE TABLE command

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,4 @@
--- Select the database
 USE alx_book_store;
 
--- Display the full description of the 'books' table without using DESCRIBE or EXPLAIN
-SHOW CREATE TABLE Books;
+SHOW CREATE TABLE books;
+
