cc8b7a117cfd60aa837cc0d42f1a6b8fb38e346a
Lerroy Gatimu <lerroygatimu@gmail.com>
2025-10-19 12:17:44 +0300

    Update comment for clarity on books table structure

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
 -- Select the database
 USE alx_book_store;
 
--- Show the full structure of the 'books' table without using DESCRIBE or EXPLAIN
+-- Display the full description of the 'books' table without using DESCRIBE or EXPLAIN
 SHOW CREATE TABLE Books;
